b575f75769af9eedeb0a713f6caca7f485e28539
gustavvega <gustav.vega@gmail.com>
2026-02-11 12:38:48 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "qa-hcg-v2";
+const CACHE_NAME = "qa-hcg-v3";
 const ASSETS = ["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", (event) => {
